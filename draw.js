1d70b13494aa024e4e7ce79227190000d9525e9e
John McLear <john@mclear.co.uk>
2020-11-22 18:21:46 +0000

    autofixes from Etherpad checkPlugins.js

--- draw.js
+++ draw.js
@@ -1,58 +1,57 @@
-var eejs = require('ep_etherpad-lite/node/eejs/');
-var settings = require('ep_etherpad-lite/node/utils/Settings');
-var two_editorsString = "";
+const eejs = require('ep_etherpad-lite/node/eejs/');
+const settings = require('ep_etherpad-lite/node/utils/Settings');
+const two_editorsString = '';
 
 exports.eejsBlock_editbarMenuLeft = function (hook_name, args, cb) {
-  args.content = args.content + eejs.require("ep_two_editors/templates/editbarButtons.ejs");
+  args.content += eejs.require('ep_two_editors/templates/editbarButtons.ejs');
   return cb();
-}
+};
 
-exports.clientVars = function(hook, context, callback){
-  var two_editors = {};
+exports.clientVars = function (hook, context, callback) {
+  const two_editors = {};
 
   try {
-    if (settings.ep_two_editors.host){
+    if (settings.ep_two_editors.host) {
       two_editors.host = settings.ep_two_editors.host;
-    }else{
+    } else {
       console.warn("ep_two_editors.host NOT SET in settings.json.  The requirement is the host of the ethertwo_editors service IE two_editors.etherpad.org, copy/paste value to settings.json --  'ep_two_editors' { 'host': 'your.ethertwo_editorshost.com'}");
-      two_editors.host = "two_editors.etherpad.org";
+      two_editors.host = 'two_editors.etherpad.org';
     }
-  } catch (e){
+  } catch (e) {
     console.warn("ep_two_editors.host NOT SET in settings.json.  The requirement is the host of the ethertwo_editors service IE two_editors.etherpad.org, copy/paste value to settings.json --  'ep_two_editors' { 'host': 'your.ethertwo_editorshost.com'}");
-    two_editors.host = "two_editors.etherpad.org";
+    two_editors.host = 'two_editors.etherpad.org';
   }
 
   try {
-    if(settings.ep_two_editors.onByDefault){
+    if (settings.ep_two_editors.onByDefault) {
       two_editors.onByDefault = true;
-    }else{
+    } else {
       two_editors.onByDefault = false;
     }
-  } catch (e){
+  } catch (e) {
     two_editors.onByDefault = false;
   }
 
   try {
-    if(settings.ep_two_editors.icon){
+    if (settings.ep_two_editors.icon) {
       two_editors.icon = settings.ep_two_editors.icon;
-    }else{
-      two_editors.icon = "../static/plugins/ep_two_editors/static/img/icon.png";
+    } else {
+      two_editors.icon = '../static/plugins/ep_two_editors/static/img/icon.png';
     }
-  } catch (e){
-    two_editors.icon = "../static/plugins/ep_two_editors/static/img/icon.png";
+  } catch (e) {
+    two_editors.icon = '../static/plugins/ep_two_editors/static/img/icon.png';
   }
-    
+
   try {
-    if(settings.ep_two_editors.position){
+    if (settings.ep_two_editors.position) {
       two_editors.position = settings.ep_two_editors.position;
-    }else{
-      two_editors.position = "left";
+    } else {
+      two_editors.position = 'left';
     }
-  } catch (e){
-    two_editors.position = "right";
+  } catch (e) {
+    two_editors.position = 'right';
   }
 
 
-  return callback( { "ep_two_editors": two_editors } );
+  return callback({ep_two_editors: two_editors});
 };
-
--- static/js/two_editors.js
+++ static/js/two_editors.js
@@ -1,64 +1,62 @@
-var postAceInit = function(hook, context){
-  var two_editors = clientVars.ep_two_editors;
-  if(two_editors){
-    if(two_editors.onByDefault){ // Setup testing else poop out
-      if(two_editors.onByDefault){
+var postAceInit = function (hook, context) {
+  const two_editors = clientVars.ep_two_editors;
+  if (two_editors) {
+    if (two_editors.onByDefault) { // Setup testing else poop out
+      if (two_editors.onByDefault) {
         enabletwo_editors();
         showtwo_editors();
       }
-    }
-    else{
-      $("#two_editors").hide();
+    } else {
+      $('#two_editors').hide();
       clientVars.ep_two_editors.enabled = false;
       // we don't two_editors it by default
     }
 
-    $('.toggle_two_editors').click(function() {
+    $('.toggle_two_editors').click(() => {
       toggletwo_editors();
     });
   }
 
   try {
-    if (clientVars.ep_two_editors.icon){
-      $('.toggle_two_editors').css("background-image", 'url('+clientVars.ep_two_editors.icon+')');
+    if (clientVars.ep_two_editors.icon) {
+      $('.toggle_two_editors').css('background-image', `url(${clientVars.ep_two_editors.icon})`);
     }
   } catch (e) {
 
   }
 
   try {
-    if (clientVars.ep_two_editors.position){
-      if(clientVars.ep_two_editors.position === "right"){
-        $('.two_editors').parent().prependTo(".menu_right");
+    if (clientVars.ep_two_editors.position) {
+      if (clientVars.ep_two_editors.position === 'right') {
+        $('.two_editors').parent().prependTo('.menu_right');
       }
-    } 
+    }
   } catch (e) {
 
   }
+};
 
-}
+function enabletwo_editors() {
+  const authorName = 'Testing';
+  const authorColor = $('#myswatch').css('background-color');
+  const two_editors_host = clientVars.ep_two_editors.host;
 
-function enabletwo_editors(){
-  var authorName = 'Testing';
-  var authorColor = $('#myswatch').css('background-color');
-  var two_editors_host = clientVars.ep_two_editors.host;
-
-  var padID = clientVars.padId;
-  console.log("prepending");
-  if($("#two_editors").length === 0){ // If it's not available already then two_editors it
-    $("#editorcontainer").prepend("<div id=two_editors><iframe id='two_editorsEmbed' src='//"+two_editors_host+"/p/"+padID+"_two_editors?useMonospaceFont=true&showControls=false&authorName="+authorName+"&authorColor="+authorColor+"' width='100%' height='100%' style='border:none' frameborder='0' scrolling='no'></iframe></div>");
+  const padID = clientVars.padId;
+  console.log('prepending');
+  if ($('#two_editors').length === 0) { // If it's not available already then two_editors it
+    $('#editorcontainer').prepend(`<div id=two_editors><iframe id='two_editorsEmbed' src='//${two_editors_host}/p/${padID}_two_editors?useMonospaceFont=true&showControls=false&authorName=${authorName}&authorColor=${authorColor}' width='100%' height='100%' style='border:none' frameborder='0' scrolling='no'></iframe></div>`);
   }
   clientVars.ep_two_editors.enabled = true;
   showtwo_editors();
 }
 
-function showtwo_editors(){
-  $('iframe[name="ace_outer"]').css("width","50%");
-  $("#two_editors").css({"z-index":"999999", "position":"absolute", "top":"0px", "right":"0px", "height":"100%", "width":"50%", "border-left":"1px solid #ccc"}).show();
-  $("#two_editorsEmbed").show().css({"overflow":"hidden"});
-  if(clientVars.ep_two_editors.enabled !== true){
+function showtwo_editors() {
+  $('iframe[name="ace_outer"]').css('width', '50%');
+  $('#two_editors').css({'z-index': '999999', 'position': 'absolute', 'top': '0px', 'right': '0px', 'height': '100%', 'width': '50%', 'border-left': '1px solid #ccc'}).show();
+  $('#two_editorsEmbed').show().css({overflow: 'hidden'});
+  if (clientVars.ep_two_editors.enabled !== true) {
     enabletwo_editors();
-/*
+    /*
     $("#two_editors").hover(function(){
       clearTimeout($(this).data('timeout'));
       $("#two_editors").animate({"width":"100%", "height": "100%"});;
@@ -75,30 +73,30 @@ function showtwo_editors(){
   clientVars.ep_two_editors.visible = true;
 }
 
-function hidetwo_editors(){
-  $("#two_editors").hide();
+function hidetwo_editors() {
+  $('#two_editors').hide();
   clientVars.ep_two_editors.fullscreen = false;
   clientVars.ep_two_editors.visible = false;
 }
 
-function toggletwo_editors(){
-  if(clientVars.ep_two_editors.visible === true && clientVars.ep_two_editors.fullscreen){
+function toggletwo_editors() {
+  if (clientVars.ep_two_editors.visible === true && clientVars.ep_two_editors.fullscreen) {
     hidetwo_editors();
     return;
   }
-  if(!clientVars.ep_two_editors.visible){
+  if (!clientVars.ep_two_editors.visible) {
     showtwo_editors();
     return;
   }
-  if(clientVars.ep_two_editors.visible === true && !clientVars.ep_two_editors.fullscreen){
+  if (clientVars.ep_two_editors.visible === true && !clientVars.ep_two_editors.fullscreen) {
     fullScreentwo_editors();
     return;
   }
 }
 
-function fullScreentwo_editors(){
+function fullScreentwo_editors() {
   clientVars.ep_two_editors.fullscreen = true;
-  $("#two_editors").animate({"width":"100%", "height": "100%"});;
+  $('#two_editors').animate({width: '100%', height: '100%'});
 }
 
 exports.postAceInit = postAceInit;
